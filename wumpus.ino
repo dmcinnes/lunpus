9cde378f3ceb14deb79e481583ef5820c3bc92a7
Doug McInnes <doug@dougmcinnes.com>
2021-10-12 21:29:10 -0700

    buttons working

--- wumpus.ino
+++ wumpus.ino
@@ -4,10 +4,17 @@
 #define SHIFT_PIN_STCP 4
 #define SHIFT_PIN_DS   3
 
+enum button{north, south, east, west};
+
+// north, south, east, west
+const uint8_t buttonPins[4] = {9, 8, 6, 10};
+
 const uint8_t northWall[2] = {0x01, 0x81};
 const uint8_t southWall[2] = {0x88, 0x08};
 const uint8_t eastWall[2]  = {0x00, 0x06};
 const uint8_t westWall[2]  = {0x30, 0x00};
+
+uint16_t buttonStates[4] = {};
 
 SevSegShift sevsegshift(
                   SHIFT_PIN_DS,
@@ -36,19 +43,87 @@ void setup() {
   updateWithDelays, leadingZeros, disableDecPoint);
   sevsegshift.setBrightness(90);
 
-  tone(7, 440);
-  delay(150);
-  tone(7, 494);
-  delay(150);
-  tone(7, 523);
-  delay(300);
-  noTone();
+  for (uint8_t i = 0; i < 4; i++) {
+    pinMode(buttonPins[i], INPUT_PULLUP);
+  }
+
+  pinMode(7, OUTPUT);
+
+  /*
+  for (int i = 0; i < 10; i++) {
+    tone(7, 31);
+    delay(100);
+    tone(7, 40);
+    delay(100);
+  }
+  for (int i = 0; i < 10; i++) {
+    tone(7, 50);
+    delay(50);
+    tone(7, 48);
+    delay(100);
+  }
+  for (int i = 0; i < 10; i++) {
+    tone(7, 31);
+    delay(100);
+    tone(7, 40);
+    delay(100);
+  }
+  */
+
+  /* tone(7, 174); */
+  /* delay(300); */
+  /* tone(7, 208); */
+  /* delay(300); */
+  /* tone(7, 220); */
+  /* delay(300); */
+  /* tone(7, 247); */
+  /* delay(300); */
+  /* tone(7, 277); */
+  /* delay(300); */
+  /* tone(7, 220); */
+  /* delay(300); */
+  /* tone(7, 277); */
+  /* delay(700); */
+  /*  */
+  /* tone(7, 261); */
+  /* delay(300); */
+  /* tone(7, 208); */
+  /* delay(300); */
+  /* tone(7, 261); */
+  /* delay(700); */
+  /*  */
+  /* tone(7, 247); */
+  /* delay(300); */
+  /* tone(7, 196); */
+  /* delay(300); */
+  /* tone(7, 247); */
+  /* delay(700); */
+  /* noTone(); */
+}
+
+bool buttonState(button id) {
+  // debounce state
+  buttonStates[id] = (buttonStates[id] << 1) | digitalRead(buttonPins[id]) | 0xe000;
+  return (buttonStates[id] == 0xf000);
 }
 
 void loop() {
   static unsigned long timer = millis();
   static int deciSeconds = 0;
   static int wall = 0;
+
+  if (buttonState(north)) {
+    sevsegshift.setChars("N");
+  }
+  if (buttonState(south)) {
+    sevsegshift.setChars("S");
+  }
+  if (buttonState(east)) {
+    sevsegshift.setChars("E");
+  }
+  if (buttonState(west)) {
+    sevsegshift.setChars("uu");
+  }
 
   if (millis() - timer >= 100) {
     timer += 100;
